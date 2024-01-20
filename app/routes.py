fd888daf7d9044a3f4f3cf183e11cafe20b6026f
xccxcxxc <chenx212@gmail.com>
2024-01-20 22:17:41 +0800

    完成 feed 文件的发布

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
+from flask import render_template, send_file, send_from_directory
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/feed')
+def feed():
+    # 提供 rss_feed.xml 文件
+    return send_from_directory('templates', 'rss_feed.xml')
