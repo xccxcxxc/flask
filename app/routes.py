5919cbae97ea5fc2dbee6f25df8cc12a98ea7b08
xccxcxxc <chenx212@gmail.com>
2024-01-23 20:55:00 +0800

    第四章 数据库， 生成 migration

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
-from flask import render_template, send_file, send_from_directory
+from flask import render_template, send_from_directory, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
+@app.route('/feed')
+def feed():
+    # 提供 rss_feed.xml 文件
+    return send_from_directory('templates', 'rss_feed.xml')
 
 @app.route('/')
 @app.route('/index')
@@ -18,7 +24,12 @@ def index():
     return render_template('index.html', title='Home', user=user,
                            posts=posts)
 
-@app.route('/feed')
-def feed():
-    # 提供 rss_feed.xml 文件
-    return send_from_directory('templates', 'rss_feed.xml')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Lgin requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign in', form=form)
