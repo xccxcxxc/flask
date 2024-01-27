8cdb91c273fd62f15485da6d2d6db40a61bef6f7
xccxcxxc <chenx212@gmail.com>
2024-01-27 12:04:49 +0800

    第五章 用户登陆 除注册外功能

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
-from flask import render_template, send_from_directory, flash, redirect, url_for
-from app import app
+from flask import render_template, send_from_directory, flash, redirect, url_for, request
+from urllib.parse import urlsplit
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app import app,db
+from app.models import User
 from app.forms import LoginForm
 
 @app.route('/feed')
@@ -9,8 +13,8 @@ def feed():
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'cc'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -21,15 +25,31 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user,
-                           posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Lgin requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
-        ))
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        print(f'next_page is {next_page}')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
